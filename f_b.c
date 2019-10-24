b0ea311b382bfe81e4a2f08c5d7d99d58cf28dc5
Gilberto Lopes <gilbertolif@dcc.ufrj.br>
2019-10-24 20:42:18 -0300

    Aplicações de teste comparando todos os algoritmos

--- f_b.c
+++ f_b.c
@@ -5,10 +5,12 @@
 #include "includes/funcs.h"
 #include "includes/inteseq.h"
 #include "includes/inteconc.h"
+#include "includes/inteconc_div.h"
 
 int main(int argc, char *argv[]) {
-  double a, b, e, resultado_seq, resultado_conc;
-  double comeco, fim, tempo_concorrente, tempo_sequencial;
+  double a, b, e, resultado_seq, resultado_conc, resultado_conc_div;
+  double comeco, fim, tempo_concorrente, tempo_concorrente_div, tempo_sequencial;
+  double (*f)(double);
   int nThreads;
 
   if (argc < 5) {
@@ -20,18 +22,26 @@ int main(int argc, char *argv[]) {
   a = atof(argv[2]);
   b = atof(argv[3]);
   e = atof(argv[4]);
+  f = f_b;
 
   GET_TIME(comeco);
-  resultado_conc = integral_conc(nThreads, a, b, e, f_b);
+  resultado_conc = integral_conc(nThreads, a, b, e, f);
   GET_TIME(fim);
   tempo_concorrente = fim - comeco;
 
   GET_TIME(comeco);
-  resultado_seq = integral_seq(a, b, e, f_b);
+  resultado_seq = integral_seq(a, b, e, f);
   GET_TIME(fim);
   tempo_sequencial = fim - comeco;
 
+  GET_TIME(comeco);
+  resultado_conc_div = integral_conc_div(nThreads, a, b, e, f);
+  GET_TIME(fim);
+  tempo_concorrente_div = fim - comeco;
+
   printf("Comparativo da função (b):\n");
+  printf("Resultado concorrente com divisão inicial: %.9lf\n", resultado_conc_div);
+  printf("Tempo de execução concorrente com divisão inicial: %.9lf\n", tempo_concorrente_div);
   printf("Resultado concorrente: %.9lf\n", resultado_conc);
   printf("Tempo de execução concorrente: %.9lf\n", tempo_concorrente);
   printf("Resultado sequencial: %.9lf\n", resultado_seq);
